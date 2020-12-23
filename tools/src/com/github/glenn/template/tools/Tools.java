b8606894611c7be5ecf95776bb60b6e9d198cded
GlennFolker <glenn.folk08@gmail.com>
2020-12-23 22:29:02 +0700

    they said groovy was good

---
+++ tools/src/com/github/glenn/template/tools/Tools.java
@@ -0,0 +1,11 @@
+package com.github.glenn.template.tools;
+
+public class Tools {
+    /**
+     * For future uses
+     * @param args Tools arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
